8cbfac23ba3f55b655ac920175748f60a12ec282
laskar-ksatria <dock_fest@yahoo.co.id>
2023-01-15 20:06:10 +0700

    chore: update

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>ABOUT PAGE</h1>
+      <Link href="/">HOME</Link>
+    </div>
+  );
+};
+
+export default AboutPage;
